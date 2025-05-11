3a40041990ddc2a2fa0c494f24363e65ae91c409
mbrkr <michael.barker1999@gmail.com>
2025-05-11 19:44:00 -0400

    Add initial Projects and Skills sections.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar';
 import About from './components/About';
+import Projects from './components/Projects'
+import Skills from './components/Skills';
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <NavBar />
       <About />
+      <Projects />
+      <Skills />
     </main>
   )
 }
---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    title: "Catacomb",
+    subtitle: "Cryptocurrency Tracker",
+    description:
+      "A simple application that tracks the latest crypto trends. Complete with recent price change line graphs.",
+    image: "./res/wallet.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "EchoJournal",
+    subtitle: "Mood Journal w/ Voice Recording",
+    description:
+      "Record messages and track your moods!",
+    image: "./project-2.gif",
+    link: "https://reedbarger.com",
+  }
+];
+
+export const skills = [
+  "Android SDK",
+  "Kotlin",
+  "Java",
+  "Android Jetpack",
+  "Jetpack Compose",
+  "MVVM"
+]
