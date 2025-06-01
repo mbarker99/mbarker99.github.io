890341a886a8ae3afd005de29f0f26bc2c8fe77c
mbrkr <michael.barker1999@gmail.com>
2025-06-01 19:01:45 -0400

    Integrate Google Analtyics for visitor tracking.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import SmartphoneMockup from './components/SmartphoneMockup';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import ReactGA from 'react-ga4';
 
 export default function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-K3562W4X26', { debug: true }); // Replace with your real ID
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+  }, []);
+
   return (
     <main className="background-pattern min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-4">
-    
       {/* Smartphone Mockup */}
       <SmartphoneMockup />
 
-      {/* Quick Links Section */}
       <div className="flex flex-col items-center space-y-2 mt-4">
         <h2 className="text-lg font-semibold text-gray-800">Quick Links</h2>
         <div className="flex space-x-6">
